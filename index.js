b16b93f86d230fb018bfb30f1e25053d27920162
wuda20 <daniel.wu35@qmail.cuny.edu>
2021-07-01 18:46:18 -0400

    server seems to work

--- index.js
+++ index.js
@@ -2,13 +2,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/', (rq, rs) => {
+  console.log(rq.body);
+});
+// app.listen(port, () => {
+//   console.log(`new app running on port ${port}`);
+// });
+
+console.log(`Hellor Node.js v${process.versions.node}!`);
 
 app.listen(port, () => {
   console.log(`new app running on port ${port}`);
 });
-
-console.log(`Hello Node.js v${process.versions.node}!`);
-
-// app.listen(port, () => {
-//   console.log(`new app running on port ${port}`);
-// });
