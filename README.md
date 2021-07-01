66e9bdc6919fef0f599fb4d0041f210793caaadb
wuda20 <daniel.wu35@qmail.cuny.edu>
2021-07-01 12:57:08 -0400

    3 files modified, 1 added

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # node-yjcncd
+Testing out Node on StackBlitz container
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-yjcncd)
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // run `node index.js` in the terminal
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`new app running on port ${port}`);
+});
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+
+// app.listen(port, () => {
+//   console.log(`new app running on port ${port}`);
+// });
